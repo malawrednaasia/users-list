d8045b75e62a79a29ea3d2e8a093a6d210949bb4
malawrednaasia <joanna.zawadzka5@gmail.com>
2018-10-11 20:08:38 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+interface User {
+  id: number;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
+  users;
   title = 'app';
+  private apiUrl = 'https://reqres.in/api/users';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get(this.apiUrl)
+      .subscribe(d => {
+        this.users = d;
+      });
+  }
 }
